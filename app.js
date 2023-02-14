15c2bc613e452c45572c8aa386dcf404d39ae7d6
JoseCoelho25 <zenani87@hotmail.com>
2023-02-14 13:30:11 +0000

    handlebars template engine

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');  //<== this setup allows me to use template engines installed by me, to help me use data
+app.engine('handlebars', expressHbs({layoutDir: 'views/layouts', defaultLayout:'main-layout', extname: 'handlebars'}));
+app.set('view engine', 'handlebars')
+//app.set('view engine', 'pug');  //<== this setup allows me to use template engines installed by me, to help me use data
 //app.set('views','template');  <== i can put the html files i create in witchever file i decide to name, but i need to define that in my main.js with this app.set
 
 const adminData = require('./routes/admin.js');
--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,15 @@ const router = express.Router();
 
 router.get('/',(req, res, next)=>{
     const products = adminData.products;
-    res.render('shop', {prods: products, pageTitle: 'Shop', path:'/'});  //<== this allows to display the shop.pug html. No need to add another path since we already in the views folder and we declared that in the app.js
+    res.render('shop', {
+        prods: products, 
+        pageTitle: 'Shop', 
+        path:'/', 
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true,
+        //layout: false  <== special key recognized by handlebars that allows to ignore the main layout
+    });  //<== this allows to display the shop.pug html. No need to add another path since we already in the views folder and we declared that in the app.js
 
     //console.log('shop.js',adminData.products);  //<== here we can display the data received by our input.
     //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
