72f06bc1922b37649e752180ca9d0d2dcd3bcccf
JoseCoelho25 <zenani87@hotmail.com>
2023-02-17 11:56:15 +0000

    admin post edit fix

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express();
 
 //app.engine('handlebars', expressHbs({layoutDir: 'views/layouts', defaultLayout:'main-layout', extname: 'handlebars'})); <== setup engine for handlebars
 app.set('view engine', 'ejs')
+app.set('views', 'views');
 //app.set('view engine', 'pug');  //<== this setup allows me to use template engines installed by me, to help me use data
 //app.set('views','template');  <== i can put the html files i create in witchever file i decide to name, but i need to define that in my main.js with this app.set
 
--- controllers/admin.js
+++ controllers/admin.js
@@ -1,8 +1,11 @@
+
 const Product = require('../models/product');
+
+
 
 exports.getAddProduct = (req, res, next)=>{
     res.render('admin/edit-product', {
-        pageTitle: 'Products Page', 
+        pageTitle: 'Add Product', 
         path: '/admin/add-product',
         editing: false
     });
@@ -26,55 +29,71 @@ exports.postAddProduct = (req, res, next) => {
         });
   };
 
-// exports.getEditProduct = (req, res, next)=>{
-//     const editMode = req.query.edit;
-//     if(!editMode){
-//         return res.redirect('/');
-//     }
-//     const prodId = req.params.productId;
-//     Product.findById(prodId, product => {
-//         if (!product) {
-//             return res.redirect('/');
-//         }
-//         res.render('admin/edit-product', {
-//         pageTitle: 'Edit Product', 
-//         path: '/admin/edit-product',
-//         editing: editMode,
-//         product: product
-//         });
-//     });
-// };
+  exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if (!editMode) {
+      return res.redirect('/');
+    }
+    const prodId = req.params.productId;
+    Product.findById(prodId)
+      // Product.findById(prodId)
+      .then(product => {
+        if (!product) {
+          return res.redirect('/');
+        }
+        res.render('admin/edit-product', {
+          pageTitle: 'Edit Product',
+          path: '/admin/edit-product',
+          editing: editMode,
+          product: product
+        });
+      })
+      .catch(err => console.log(err));
+  };
 
-// exports.postEditProduct = (req, res, next) => {
-//     const prodId = req.body.productId;
-//     const updatedTitle = req.body.title;
-//     const updatedImageUrl = req.body.imageUrl;
-//     const updatedPrice = req.body.price;
-//     const updatedDesc = req.body.description;
-//     const updatedProduct = new Product(
-//         prodId, 
-//         updatedTitle, 
-//         updatedImageUrl, 
-//         updatedDesc,
-//         updatedPrice
-//     );
-    
-//      updatedProduct.save();
-//      res.redirect('/admin/products');
-// }
+  exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDesc = req.body.description;
 
-// exports.getProducts = (req, res ,next) => {
-//     Product.fetchAll(products => {
-//         res.render('admin/products', {
-//         prods: products,
-//         pageTitle: 'Admin Products', 
-//         path: '/admin/products',
-//         });
-//     }) 
-// }
+    const product = new Product(
+        updatedTitle, 
+        updatedPrice, 
+        updatedDesc, 
+        updatedImageUrl,
+        prodId
+        );
+      product.save().then(result => {
+        console.log(product);
+        console.log('UPDATED PRODUCT!');
+        res.redirect('/admin/products');
+      })
+      .catch(err => console.log(err));
+  };
+
+ exports.getProducts = (req, res ,next) => {
+     Product.fetchAll()
+        .then(products => {
+            res.render('admin/products', {
+                prods: products,
+                pageTitle: 'Admin Products', 
+                path: '/admin/products',
+            });
+        }) 
+        .catch(err => console.log(err));
+ }
 
 // exports.postDeleteProduct = (req, res, next) => {
-//     const prodId = req.body.productId;
-//     Product.deleteById(prodId);
-//     res.redirect('/admin/products');
-// }
+//   const prodId = req.body.productId;
+//   Product.findById(prodId)
+//     .then(product => {
+//       return product.destroy();
+//     })
+//     .then(result => {
+//       console.log('DESTROYED PRODUCT');
+//       res.redirect('/admin/products');
+//     })
+//     .catch(err => console.log(err));
+// };
