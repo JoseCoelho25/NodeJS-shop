f896d74144adc5e4ffddeb8d42a615420f5d7a40
JoseCoelho25 <zenani87@hotmail.com>
2023-02-21 21:36:17 +0000

    user data stored in mongodb sessions

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://jose:zemaior25@cluster0.hz58fyg.mongodb.net/shop?retryWrites=true&w=majority';
+
 const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+});
 
 
 app.set('view engine', 'ejs')
@@ -22,8 +29,12 @@ const authRoutes = require('./routes/auth.js');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-    session({secret: 'my secret', resave: false, saveUninitialized: false})
-    ;)
+    session({
+        secret: 'my secret', 
+        resave: false, 
+        saveUninitialized: false, 
+        store: store
+    }));
 
  app.use((req, res, next) => {  //to find userById
      User.findById('63f162e7372ded7a1d9c0f0d')
@@ -44,7 +55,7 @@ app.use(errorController.get404)
 mongoose.set('strictQuery', false);
 
 
-mongoose.connect('mongodb+srv://jose:zemaior25@cluster0.hz58fyg.mongodb.net/shop?retryWrites=true&w=majority')
+mongoose.connect(MONGODB_URI)
 .then(result => {
     User.findOne().then(user => {
         if (!user) {
