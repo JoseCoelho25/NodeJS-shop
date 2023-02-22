3d868bb2f3ba82b8b9e26cf87930e544ec3aeae3
JoseCoelho25 <zenani87@hotmail.com>
2023-02-22 23:30:56 +0000

    added protection to post actions

--- controllers/admin.js
+++ controllers/admin.js
@@ -66,21 +66,23 @@ exports.postAddProduct = (req, res, next) => {
 
     Product.findById(prodId)
       .then(product => {
+        if (product.userId.toString() !== req.user._id.toString()){
+          return res.redirect('/');
+        }
         product.title = updatedTitle;
         product.price = updatedPrice;
         product.description = updatedDesc;
         product.imageUrl = updatedImageUrl;
-        return product.save();
-      })
-      .then(result => {
+        return product.save().then(result => {
         console.log('UPDATED PRODUCT!');
         res.redirect('/admin/products');
+        })
       })
       .catch(err => console.log(err));
   };
 
  exports.getProducts = (req, res ,next) => {
-     Product.find()
+     Product.find({userId: req.user._id})
         //.select('title price -_id') //this allows me to get specific data with having to query data
         //.populate('userId') //this allows me to get the full data from the user and not only the id
         .then(products => {
@@ -95,7 +97,7 @@ exports.postAddProduct = (req, res, next) => {
 
  exports.postDeleteProduct = (req, res, next) => {
    const prodId = req.body.productId;
-    Product.findByIdAndRemove(prodId)
+    Product.deleteOne({_id: prodId, userId: req.user._id})
      .then(() => {
        console.log('DESTROYED PRODUCT');
        res.redirect('/admin/products');
