00a6922164a4bc6263c05842801f61973f83961e
JoseCoelho25 <zenani87@hotmail.com>
2023-02-22 23:56:53 +0000

    added signup validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendGridTransport = require('nodemailer-sendgrid-transport');
+const { validationResult } = require('express-validator');
 
 const transporter = nodemailer.createTransport(sendGridTransport({
   auth: {
@@ -80,6 +81,14 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg
+    });
+  }
   User.findOne({ email: email})
   .then(userDoc => {
     if (userDoc) {
