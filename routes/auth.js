a705a710974cdbe75eb4aa9931bf5814bb8f01d7
JoseCoelho25 <zenani87@hotmail.com>
2023-02-23 12:02:28 +0000

    sync

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -9,7 +10,19 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post(
+    "/login",
+    [
+      check("email").isEmail().withMessage("Please enter a valid email"),
+      check(
+        "password",
+        "Please enter a password with only numbers and text and at least 5 characters."
+      )
+        .isLength({ min: 5 })
+        .isAlphanumeric(),
+    ],
+    authController.postLogin
+  );
 
 router.post(
     '/signup',
@@ -18,11 +31,17 @@ router.post(
         .isEmail()
         .withMessage('Please enter a valid email.')
         .custom((value, { req }) => {
-          if (value === 'test@test.com') {
-            throw new Error('This email address if forbidden.');
-          }
-          return true;
-        }),
+        //   if (value === 'test@test.com') {
+        //     throw new Error('This email address if forbidden.');
+        //   }
+        //   return true;
+        return User.findOne({ email: value})
+        .then(userDoc => {
+            if (userDoc) {
+              return Promise.reject('E-mail already exists, please pick a different one!')
+            }
+        })
+    }),
       body(
         'password',
         'Please enter a password with only numbers and text and at least 5 characters.'
