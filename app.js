019495e264c719c1124132c1598b7b815e630039
JoseCoelho25 <zenani87@hotmail.com>
2023-02-23 20:29:21 +0000

    throw error verifications

--- app.js
+++ app.js
@@ -42,29 +42,48 @@ app.use(
 
 app.use(csrfProtection);
 
- app.use((req, res, next) => {  
-    if (!req.session.user) {
-        return next();
-      }
-      User.findById(req.session.user._id)
-        .then(user => {
-          req.user = user;
-          next();
-        })
-        .catch(err => console.log(err));
-    });
-
 app.use((req, res, next) => {
     res.locals.isAuthenticated = req.session.isLoggedIn;
     res.locals.csrfToken = req.csrfToken();
     next();
 })
 
+app.use((req, res, next) => {  
+    //return new Error('Dummy);  to simulate a error on login to display code500
+    if (!req.session.user) {
+        return next();
+    }
+    User.findById(req.session.user._id)
+        .then(user => {
+            if(!user) {
+                return next();
+            }
+        req.user = user;
+        next();
+        })
+        .catch(err => {
+            next(new Error(err));
+        });
+    });
+
+
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.get('/500', errorController.get500);
+
 app.use(errorController.get404)
+
+app.use((error, req, res, next) => { //express has this 4th parameter - error, that ignores its middleware position. When it catches a .catch with an throw(error) inside, it gives priority to that over every middleware.
+    //res.redirect('/500');
+    // res.status(error.httpStatusCode).render(...);
+    res.status(500).render('500', {
+        pageTitle: 'Error!',
+        path: '/500',
+        isAuthenticated: req.session.isLoggedIn
+    });
+})
 
 // Set the 'strictQuery' option to 'false' to avoid deprecation warning
 mongoose.set('strictQuery', false);
