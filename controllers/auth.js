64efb11d3e5c0f3149839d4121c42d0320e2548a
JoseCoelho25 <zenani87@hotmail.com>
2023-02-23 11:45:22 +0000

    login password validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,6 +46,15 @@ exports.getLogin = (req, res, next) => {
   exports.postLogin = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render('auth/login', {
+        path: '/login',
+        pageTitle: 'Login',
+        errorMessage: errors.array()[0].msg
+      });
+    }
     User.findOne({ email: email})
     .then(user => {
       if (!user) {
@@ -80,7 +89,7 @@ exports.getLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
+
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).render('auth/signup', {
@@ -89,13 +98,7 @@ exports.postSignup = (req, res, next) => {
       errorMessage: errors.array()[0].msg
     });
   }
-  User.findOne({ email: email})
-  .then(userDoc => {
-    if (userDoc) {
-      req.flash('error','Email exists already. Please pick a different one');
-      return res.redirect('/signup');
-    }
-    return bcrypt
+     bcrypt
     .hash(password, 12)
     .then(hashedPassword => {
       console.log(hashedPassword)
@@ -115,11 +118,9 @@ exports.postSignup = (req, res, next) => {
         html: '<h1>You successfully signed up!</h1>'
       })
     })
-    .catch(err => {console.log(err)})
-  })
-  .catch(err => {
-    console.log(err)
-  });
+    .catch(err => {
+      console.log(err);
+    })
 };
 
 exports.postLogout = (req, res, next) => {
